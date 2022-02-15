f5596015c9336bee8bd43d106ba59c961f6a6bca
ALOK KUMAR PANDEY <alokpandey2095@gmail.com>
2022-02-15 15:00:44 +0530

    facebook afterlogin page using html,css,javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+var commentSectionEl=document.getElementById('commentSection');
+
+
+function commentSection(){
+    commentSectionEl.classList.toggle('comment_section_display');
+}
+
+
+var shareEl=document.getElementById('share');
+
+function share(){
+    shareEl.classList.toggle('share_display')
+}
